a58d5221288ef1c7bca5b0a5aa462df882f0a1dc
milan9kro <137121590+milan9kro@users.noreply.github.com>
2023-12-04 02:03:16 +0100

    Add files via upload

--- zalohuj.php
+++ zalohuj.php
@@ -1,24 +1,71 @@
-// Pripojnie do databazy : $conn
-// a SSH pripojenie k serveru s názvom $ssh
+ï»¿<?php
 
-// cesta pre zálohy
+define('IN_CODE', true);
+include_once("config.php");
+include_once("myweb.php");
+
+if ($GLOBALS["prmyctenar"]->ctenarstav == 0) {
+    $GLOBALS['vzhledwebu']->Generuj();
+    ObrTabulka();
+    header("Location:" . $GLOBALS['baseurl'] . "readers.php");
+    die;
+}
+
+$GLOBALS['vzhledwebu']->Generuj();
+ObrTabulka();
+
+$conn = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname);
+mysqli_set_charset($conn, "utf8");
+if ($conn->connect_error) {
+    die("Pripojenie zlyhalo: " . $conn->connect_error);
+}
+
+require 'vendor/autoload.php';
+
+use phpseclib3\Net\SSH2;
+
+$hostname = 'xxxxx';
+$username = 'u9695';
+$password = 'xxxxxx';
+$port = 318;
+
+$ssh = new SSH2($hostname, $port);
+
+if (!$ssh->login($username, $password)) {
+    exit('Nepodarilo sa pripojiÅ¥ cez SSH. Chyba: ' . $ssh->getLastError());
+}
+
+
+
+// Pripojnie do databazy : $conn
+// a SSH pripojenie k serveru s nÃ¡zvom $ssh
+
+// cesta pre zÃ¡lohy
 $backupDirectory = 'public_html/zalohy/01';
-// Pripojenie cez SSH, vytvor adresár
+// Pripojenie cez SSH, vytvor adresÃ¡r
 $ssh->exec("mkdir -p $backupDirectory");
-// Získaj dátum a èas pre názov súboru
+// ZÃ­skaj dÃ¡tum a Äas pre nÃ¡zov sÃºboru
 $backupFileName = 'backup_' . date('Ymd_His') . '.sql';
-// Cesta k záloze na vzdáleném serveru
+// Cesta k zÃ¡loze na vzdÃ¡lenÃ©m serveru
 $backupFilePath = $backupDirectory . '/' . $backupFileName;
 // tabulky pre zalohovanie
 $tables = array('v_ule', 'v_stanovistia');
-// Príkaz pre vytvorenie zalohy
+// PrÃ­kaz pre vytvorenie zalohy
 $command = "mysqldump -h $dbserver -u $dbuser -p$dbpass $dbname " . implode(' ', $tables) . " > $backupFilePath";
-// spusti príkaz cez  SSH
+// spusti prÃ­kaz cez  SSH
 $ssh->exec($command);
 
-// Pripojenie cez SSH a zistenie obsahu adresára
+// Pripojenie cez SSH a zistenie obsahu adresÃ¡ra
 $ssh->exec("ls -l $backupDirectory");
 
-echo 'Záloha bola úspešne vytvorená a uložená v ' . $backupFilePath;
+echo 'ZÃ¡loha bola ÃºspeÅ¡ne vytvorenÃ¡ a uloÅ¾enÃ¡ v ' . $backupFilePath;
 // Uzavretie pripojenia cez SSH
 $ssh->disconnect();
+
+    KonecObrTabulka();
+    $GLOBALS['vzhledwebu']->Generuj();
+
+    // UkonÄenie pripojenia k databÃ¡ze
+    mysqli_close($conn);
+    ?>
+
